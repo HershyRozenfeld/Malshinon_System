5c4e25c2e3ac1afca11a1f9239d08b0529419c3c
hershy rozenfeld <Y6764057@gmail.com>
2025-06-09 17:41:43 +0300

    Add GetPersonByName method to PeopelDAL.cs
    Implemented a new method `GetPersonByName` to retrieve a `People` object from a MySQL database using first and last names. Introduced a connection string variable `_connStr` for database connectivity. Added a placeholder method `GetPersonBySecretCode`. Updated the namespace from `Malshinon.DAL` to `Malshinon`.

--- DAL/PeopelDAL.cs
+++ DAL/PeopelDAL.cs
@@ -1,17 +1,57 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Malshinon.DAL
+namespace Malshinon
 {
     internal class PeopelDAL
     {
-        public void GetPersonByName()
+        private readonly string _connStr = "server=localhost;user=root;password=;database=Malshinon";
+        public People GetPersonByName(string first_name, string last_name)
         {
+            People people = null;
+            using (var conn = new MySqlConnection(_connStr))
+            {
 
+                try
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM People WHERE first_name = @first_name AND last_name = @last_name";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@first_name", first_name);
+                        cmd.Parameters.AddWithValue("@last_name", last_name);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var typeString = reader.GetString(reader.GetOrdinal("type"));
+                                PersonType type = (PersonType)Enum.Parse(typeof(PersonType), typeString, true);
+                                people = new People(
+                                    reader.GetInt32("id"),
+                                    reader.GetString("first_name"),
+                                    reader.GetString("last_name"),
+                                    reader.GetString("secret_code"),
+                                    type,
+                                    reader.GetInt32("num_reports"),
+                                    reader.GetInt32("num_mentions")
+
+                                );
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error fetching people by name: " + e.Message);
+                }
+            }
+            return people;
         }
+
 
         public void GetPersonBySecretCode()
         {
