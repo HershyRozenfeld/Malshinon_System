a58f45525f07b6b34623a645b03ee0333ad9b24d
hershy rozenfeld <Y6764057@gmail.com>
2025-06-10 15:53:18 +0300

    Add MySQL support for IntelReports insertion
    Introduced MySql.Data.MySqlClient for database operations.
    Added a private connection string and modified the
    InsertIntelReport method to accept an IntelReports
    object, implementing parameterized SQL queries and
    error handling. Removed the old method implementation
    and ensured proper resource management with using statements.

--- DAL/IntelReportsDAL.cs
+++ DAL/IntelReportsDAL.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,29 @@ namespace Malshinon.DAL
 {
     internal class IntelReportsDAL
     {
-        public void InsertIntelReport()
+        private readonly string _connStr = "server=localhost;user=root;password=;database=Malshinon";
+        public void InsertIntelReport(IntelReports intelReports)
         {
-
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = @"INSERT INTO IntelReports (reporter_id, target_id, text)
+                                  VALUES (@reporter_id, @target_id, @text)";
+                    using (var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@reporter_id", intelReports.reporterId);
+                        cmd.Parameters.AddWithValue("@target_id", intelReports.targetId);
+                        cmd.Parameters.AddWithValue("@text", intelReports.text);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error INSERT IntelReport to the DB: " + e.Message);
+                }
+            }
         }
 
         public void UpdateReportCount()
