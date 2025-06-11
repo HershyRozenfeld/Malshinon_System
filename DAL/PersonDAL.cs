5c2bbb2469ce89d25b55eccba1dd4fcbecbb34e2
hershy rozenfeld <Y6764057@gmail.com>
2025-06-11 15:49:46 +0300

    Add PersonDAL class for MySQL data access
    Introduces the `PersonDAL` class in the `Malshinon` namespace to handle data access for `Person` entities. This class includes methods for retrieving, inserting, and checking the existence of `Person` records in a MySQL database, utilizing the `MySql.Data.MySqlClient` library. Error handling is implemented to log exceptions during database operations.

---
+++ DAL/PersonDAL.cs
@@ -0,0 +1,161 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class PersonDAL
+    {
+        private readonly string _connStr = "server=localhost;user=root;password=;database=Malshinon";
+        private Person MapReaderToPeople(MySqlDataReader reader)
+        {
+            var typeString = reader.GetString(reader.GetOrdinal("type"));
+            PersonType type = (PersonType)Enum.Parse(typeof(PersonType), typeString, true);
+            return new Person(
+                reader.GetInt32("id"),
+                reader.GetString("first_name"),
+                reader.GetString("last_name"),
+                reader.GetString("secret_code"),
+                type,
+                reader.GetInt32("num_reports"),
+                reader.GetInt32("num_mentions")
+            );
+        }
+        public Person GetPersonByName(string first_name, string last_name)
+        {
+            Person person = null;
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM Person WHERE first_name = @first_name AND last_name = @last_name";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@first_name", first_name);
+                        cmd.Parameters.AddWithValue("@last_name", last_name);
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                person = MapReaderToPeople(reader);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error fetching person by name: " + e.Message);
+                }
+            }
+            return person;
+        }
+        public Person GetPersonBySecretCode(string secret_code)
+        {
+            Person person = null;
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM Person WHERE secret_code = @secret_code";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@secret_code", secret_code);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                person = MapReaderToPeople(reader);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error fetching person by secret_code: " + e.Message);
+                }
+            }
+            return person;
+        }
+        public void InsertNewPerson(Person person)
+        {
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = @"INSERT INTO Person (first_name, last_name, secret_code, type)
+                                        VALUES (@first_name, @last_name, @secret_code, @type)";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@first_name", person.firstName);
+                        cmd.Parameters.AddWithValue("@last_name", person.lastName);
+                        cmd.Parameters.AddWithValue("@secret_code", person.secretCode);
+                        cmd.Parameters.AddWithValue("@type", person.type);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error INSERT person to the DB: " + e.Message);
+                }
+            }
+        }
+        public bool PersonExistsByName(string first_name, string last_name)
+        {
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = "SELECT 1 FROM Person WHERE first_name = @first_name AND last_name = @last_name LIMIT 1";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@first_name", first_name);
+                        cmd.Parameters.AddWithValue("@last_name", last_name);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            return reader.Read();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error checking person existence by name: " + e.Message);
+                    return false;
+                }
+            }
+        }
+
+        public bool PersonExistsBySecretCode(string secret_code)
+        {
+            using (var conn = new MySqlConnection(_connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    var query = "SELECT 1 FROM Person WHERE secret_code = @secret_code LIMIT 1";
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@secret_code", secret_code);
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            return reader.Read();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error checking person existence by secret_code: " + e.Message);
+                    return false;
+                }
+            }
+        }
+
+    }
+}
