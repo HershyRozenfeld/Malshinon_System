4fe750e010aecec0f1a79a289856005c03b855b9
hershy rozenfeld <Y6764057@gmail.com>
2025-06-11 15:50:59 +0300

    Add Person class with PersonType enumeration
    Introduces a new `Person` class in the `Malshinon` namespace, featuring properties for personal details and a unique `secretCode`. Includes an enumeration `PersonType` with four types: `Reporter`, `Target`, `Both`, and `PotentialAgent`. Two constructors are provided for flexibility in object creation, along with a private method to generate the `secretCode` based on the hash of the `firstName` and `lastName`.

---
+++ Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    public enum PersonType
+    {
+        Reporter,
+        Target,
+        Both,
+        PotentialAgent
+    }
+    internal class Person
+    {
+        int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string secretCode { get; set; }
+        public PersonType type { get; set; } = PersonType.Reporter;
+        int numReports { get; set; }
+        int numMentions { get; set; }
+        public Person(int id, string firstName, string lastName, string secretCode, PersonType type, int numReports, int numMentions)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.secretCode = secretCode;
+            this.type = type;
+            this.numReports = numReports;
+            this.numMentions = numMentions;
+            SecretCodeMaker();
+        }
+        public Person(string firstName, string lastName, string secretCode)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.secretCode = secretCode;
+            SecretCodeMaker();
+        }
+        private void SecretCodeMaker()
+        {
+            string combined = firstName + lastName;
+            int hash = combined.GetHashCode();
+            this.secretCode = Math.Abs(hash).ToString();
+        }
+    }
+}
